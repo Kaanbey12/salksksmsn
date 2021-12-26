52ae6e3ba9011aa79e714aa9b128d4b4ae26f89c
Glitch (glitch-hello-node) <none>
2021-12-26 10:07:03 +0000

    ✋⛰ Checkpoint ./extrax.js:40237942/471

--- extrax.js
+++ extrax.js
@@ -21,15 +21,22 @@ $log[$userTag[$clientID] ismi ile giriş yapıldı]`
 bot.command({
   name:"kayıt",
   code:`
-  $let[a;$apiMessage[$channelID;{color:BDAAF9}{description:
-  <a:emoji_28:852134852332617748> <@$mentioned[1]> Kişisi Için Bir Kayıt Sistemi Başlatıldı!
-  }{thumbnail:$userAvatar[$mentioned[1]]}{actionRow:Erkek,2,3,erkek:Kız,2,4,kız:İptal,2,3,iptal};;yes]]
+  $let[a;$apiMessage[$channelID;{color:BDAAF9}{description:<a:emoji_28:852134852332617748> <@$mentioned[1]> Kişisi Için Bir Kayıt Sistemi Başlatıldı!}{thumbnail:$userAvatar[$mentioned[1]]};{actionRow:Erkek,2,3,erkek:Kız,2,4,kız:İptal,2,3,iptal};;yes]]
   $setUserVar[kayıtyapılan;$mentioned[1]]
+  $setUserVar[kayıtisimyaş;$message[1] | $message[2]]
   $onlyPerms[admin;$getServerVar[admin]]
+  $onlyIf[$mentioned[1]!=;{color:$getServerVar[hex]}{description:$customEmoji[yasak] | Lütfen Birini Etiketle!}]
   `
   })
 
+
+
 bot.variables({
+  kayıtisimyaş:"",
   kayıtyapılan:"",
-  admin:"{
+  hex:"BDAAF9",
+  admin:"{color:$getServerVar[hex]}{thumbnail:$userAvatar}{descripton:$customEmoji[yasak] | Üzgünüm Admin Yetkin Yok}",
+  kız:"842998514438111252",
+  erkek:"842998200050122752"
   })
+
