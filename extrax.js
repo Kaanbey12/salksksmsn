53d924cfeb99c2f64c076f3c000f9ada39cac1ca
Glitch (glitch-hello-node) <none>
2021-12-26 14:07:31 +0000

    💾🏌 Checkpoint ./extrax.js:40237942/1244

--- extrax.js
+++ extrax.js
@@ -19,38 +19,8 @@ $log[$userTag[$clientID] ismi ile giriş yapıldı]`
 }) 
 
 
-bot.command({
-  name:"kayıt",
-  code:`
-  $apiMessage[$channelID;;{color:BDAAF9}{description:<a:emoji_28:852134852332617748> <@$mentioned[1]> Kişisi Için Bir Kayıt Sistemi Başlatıldı!}{thumbnail:$userAvatar[$mentioned[1]]};{actionRow:Erkek,2,3,erkek:Kız,2,4,kız:İptal,2,3,iptal};;yes]
-  $setUserVar[kayıtyapılan;$mentioned[1];$authorID]
-  $setUserVar[kayıtisimyaş;$noMentionmessage[1] | $noMentionmessage[2];$mentioned[1]]
-  $onlyPerms[admin;$getServerVar[admin]]
-  $onlyIf[$mentioned[1]!=;{color:$getServerVar[hex]}{description:$customEmoji[yasak] | Lütfen Birini Etiketle!}]
-  $clear[1]
-  `
-  })
-bot.interactionCommand({
-  name:"erkek",
-  prototype:"button",
-  code:`
-  $interactionReply[<@$getServerVar[kayıtyapılan] Kaydı Yapılan Kişinin <@$authorID> Tarafından Kaydı Sonuçlandırıldı]
-  $giveRole[$getServerVar[kayıtyapılan];$getServerVar[erkek]]
-  $changeNickname[$getServerVar[kayıtyapılan];$noMentionmessage[1] | $noMentionmessage[2]]
-  `})
-bot.interactionCommand({
-  name:"kız",
-  prototype:"button",
-  code:`
-  $interactionReply[Test]
-  `})
-bot.interactionCommand({
-  name:"iptal",
-  prototype:"button",
-  code:`
- Kayıt İptal Edildi
-  `})
 
+  
 bot.variables({
   kayıtisimyaş:"",
   kayıtyapılan:"",
