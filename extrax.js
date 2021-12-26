d8fa5b45768e24ef1fc29ae76521fcb53a1b942a
Glitch (glitch-hello-node) <none>
2021-12-26 12:39:58 +0000

    🚝🏰 Checkpoint ./extrax.js:40237942/40

--- extrax.js
+++ extrax.js
@@ -23,8 +23,8 @@ bot.command({
   name:"kayıt",
   code:`
   $apiMessage[$channelID;;{color:BDAAF9}{description:<a:emoji_28:852134852332617748> <@$mentioned[1]> Kişisi Için Bir Kayıt Sistemi Başlatıldı!}{thumbnail:$userAvatar[$mentioned[1]]};{actionRow:Erkek,2,3,erkek:Kız,2,4,kız:İptal,2,3,iptal};;yes]
-  $setUserVar[kayıtyapılan;$mentioned[1]]
-  $setUserVar[kayıtisimyaş;$noMentionmessage[1] | $noMentionmessage[2]]
+  $setUserVar[kayıtyapılan;$mentioned[1];$authorID]
+  $setUserVar[kayıtisimyaş;$noMentionmessage[1] | $noMentionmessage[2];$mentioned[1]]
   $onlyPerms[admin;$getServerVar[admin]]
   $onlyIf[$mentioned[1]!=;{color:$getServerVar[hex]}{description:$customEmoji[yasak] | Lütfen Birini Etiketle!}]
   $clear[1]
