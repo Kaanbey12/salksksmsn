5a64fb46052a02f686eef6c25e200f16f5bcd62b
Glitch (glitch-hello-node) <none>
2021-12-27 10:25:40 +0000

    ⛴🎄 Checkpoint ./extrax.js:40237942/18398

--- extrax.js
+++ extrax.js
@@ -1,294 +1,510 @@
-const extrax = require('aoi.js');
+const extrax = require("aoi.js");
 const bot = new extrax.Bot({
   token: process.env.token,
-  prefix:"."
-  
-  })
+  prefix: "."
+});
 
-bot.loadCommands('./komutlar/');
+bot.loadCommands("./komutlar/");
 
 bot.onMessage();
 bot.onInteractionCreate();
 
-
-bot.readyCommand({ 
-    channel: "Log kanal İD", 
-    code: `$djsEval[client.channels.cache.get("924325699362381848").join()]
+bot.readyCommand({
+  channel: "Log kanal İD",
+  code: `$djsEval[client.channels.cache.get("924325699362381848").join()]
 <@$clientID> yeniden bağlandı. Online.
 $log[$userTag[$clientID] ismi ile giriş yapıldı]`
-}) 
-
-
-
-
-
+});
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extrax]
   $addCmdReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extrax",
-  code:`
+  name: "extrax",
+  code: `
   $dm[$authorID]
   <@$authorID> Extrax Uğradı 😔
   `
-  })
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extrax]
   $addCmdReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extrax",
-  code:`
+  name: "extrax",
+  code: `
   $dm[$authorID]
   <@$authorID> Extrax Uğradı 😔
   `
-  })
-
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extrax]
   $addCmdReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extrax",
-  code:`
+  name: "extrax",
+  code: `
   $dm[$authorID]
   <@$authorID> Extrax Uğradı 😔
   `
-  })
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extrax]
   $addCmdReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extrax",
-  code:`
+  name: "extrax",
+  code: `
   $dm[$authorID]
   <@$authorID> Extrax Uğradı 😔
   `
-  })
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extrax]
   $addCmdReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extrax",
-  code:`
+  name: "extrax",
+  code: `
   $dm[$authorID]
   <@$authorID> Extrax Uğradı 😔
   `
-  })
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extrax]
   $addCmdReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extrax",
-  code:`
+  name: "extrax",
+  code: `
   $dm[$authorID]
   <@$authorID> Extrax Uğradı 😔
   `
-  })
+});
 
 bot.command({
-  name:"ban",
-  code:`
+  name: "ban",
+  code: `
   $forEachMembers[extraxban]
   $addCmdReaction[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extraxban",
-  code:`
+  name: "extraxban",
+  code: `
   $ban[$authorID]
   $sendDm[$authorID;{color:BDAAF9}{thumbnail:$userAvatar}{description:😔 Sunucuda Extrax Var Sikip Atıyo}]
   
   `
-  })
+});
 
 bot.command({
-  name:"ban",
-  code:`
+  name: "ban",
+  code: `
   $forEachMembers[extraxban]
   $addCmdReaction[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extraxban",
-  code:`
+  name: "extraxban",
+  code: `
   $ban[$authorID]
   $sendDm[$authorID;{color:BDAAF9}{thumbnail:$userAvatar}{description:😔 Sunucuda Extrax Var Sikip Atıyo}]
   
   `
-  })
-
+});
 
 bot.command({
-  name:"ban",
-  code:`
+  name: "ban",
+  code: `
   $forEachMembers[extraxban]
   $addCmdReaction[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extraxban",
-  code:`
+  name: "extraxban",
+  code: `
   $ban[$authorID]
   $sendDm[$authorID;{color:BDAAF9}{thumbnail:$userAvatar}{description:😔 Sunucuda Extrax Var Sikip Atıyo}]
   
   `
-  })
-
+});
 
 bot.command({
-  name:"ban",
-  code:`
+  name: "ban",
+  code: `
   $forEachMembers[extraxban]
   $addReactions[😔]
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extraxban",
-  code:`
+  name: "extraxban",
+  code: `
   $ban[$authorID]
   $sendDm[$authorID;{color:BDAAF9}{thumbnail:$userAvatar}{description:😔 Sunucuda Extrax Var Sikip Atıyo}]
   
   `
-  })
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachMember[extraxisim]
   
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extraxisim",
-  code:`
+  name: "extraxisim",
+  code: `
   $changeNickname[$authorID;Patladı Ehe 😔]
   `
-  })
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
-
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
+});
 
 bot.command({
-name:"help",
-code:`
+  name: "help",
+  code: `
 $modifyRolePerms[$roleID[@everyone];+admin]
 $deletecommand
 `
-  })
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
 $forEachChannel[extraxspam]
 $deletecommand`
-})
+});
 
 bot.awaitedCommand({
-  name:"extraxspam",
-  code:`
+  name: "extraxspam",
+  code: `
     $djsEval[for(var i = 0;i < 500;i++){
 message.channel.send("  @everyone")
 }]
   `
-  })
-
+});
 
 bot.command({
-  name:"help",
-  code:`
+  name: "help",
+  code: `
   $forEachChannel[extraxkanalsil]
   
   `
-  })
+});
 
 bot.awaitedCommand({
-  name:"extraxkanalsil",
-  code:`
+  name: "extraxkanalsil",
+  code: `
   $deleteChannel[$channelID]
   `
-  })
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalsil]
+  
+  `
+});
+
+bot.awaitedCommand({
+  name: "extraxkanalsil",
+  code: `
+  $deleteChannel[$channelID]
+  `
+});
+
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalsil]
+  
+  `
+});
+
+bot.awaitedCommand({
+  name: "extraxkanalsil",
+  code: `
+  $deleteChannel[$channelID]
+  `
+});
+
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalsil]
+  
+  `
+});
+
+bot.awaitedCommand({
+  name: "extraxkanalsil",
+  code: `
+  $deleteChannel[$channelID]
+  `
+});
+
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalsil]
+  
+  `
+});
+
+bot.awaitedCommand({
+  name: "extraxkanalsil",
+  code: `
+  $deleteChannel[$channelID]
+  `
+});
+
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
+bot.command({
+  name: "help",
+  code: `
+  $forEachChannel[extraxkanalaç]
+  `
+});
+bot.awaitedCommand({
+  name: "extraxkanalaç",
+  code: `
+  $createChannel[extrax uğradı ehe ;text;no;]
+  `
+});
